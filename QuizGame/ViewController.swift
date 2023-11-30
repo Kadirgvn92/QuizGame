a7039d85437fb29cda916d152651baf86b1d4a1d
Kadir <kdrgvndeveloper@gmail.com>
2023-11-30 17:54:36 +0300

    Initial Commit

---
+++ QuizGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Kadir on 30.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
